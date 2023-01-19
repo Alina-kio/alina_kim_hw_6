85b4c97b3cb7596d805de7862e33ac4f8cc424bd
Alina-kio <alinaolkim@gmail.com>
2023-01-19 23:06:18 +0600

    hw on ingenuity

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,13 @@ public class Main {
         Boss boss = new Boss(200, 1200, weapon);
         System.out.println(boss.info());
 
+
+        Skeleton skeleton = new Skeleton(250, 980, weapon, 8);
+        Skeleton skeleton1 = new Skeleton(300, 1509, weapon, 16);
+        System.out.println();
+        System.out.println(skeleton.info());
+        System.out.println();
+        System.out.println(skeleton1.info());
+
     }
 }
