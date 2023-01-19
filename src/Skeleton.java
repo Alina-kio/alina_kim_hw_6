d6175bf2c92000af7eea525bd3274968e110057a
Alina-kio <alinaolkim@gmail.com>
2023-01-19 23:07:32 +0600

    hw on ingenuity

---
+++ src/Skeleton.java
@@ -0,0 +1,19 @@
+public class Skeleton extends Boss{
+    int numberOfArrows;
+
+    public Skeleton(int damage, int life, Weapon weapon, int numberOfArrows){
+        super(damage, life, weapon);
+        this.numberOfArrows = numberOfArrows;
+    }
+    public int getNumberOfArrows() {
+        return numberOfArrows;
+    }
+
+    public void setNumberOfArrows(int numberOfArrows) {
+        this.numberOfArrows = numberOfArrows;
+    }
+
+    public String info(){
+        return super.info() + "\nNumber of arrows: " + this.numberOfArrows;
+    }
+}
